8e6524ac2e9c2f23318d37818615647072ff1fc2
erfan tolou <erfantolou@gmail.com>
2026-08-21 20:19:46 +0330

    chore: initialize frontend project

---
+++ frontend/src/types/schema.ts
@@ -0,0 +1,42 @@
+export enum SourceType {
+    DIRECT = "DIRECT",
+    SOCIAL = "SOCIAL",
+    SEARCH = "SEARCH",
+    EMAIL = "EMAIL",
+    OTHER = "OTHER",
+  }
+  
+  export interface User {
+    id: number
+    createdAt: string
+    email: string
+    name: string
+    links: Link[]
+  }
+  
+  export interface Link {
+    id: number
+    createdAt: string
+    updatedAt: string
+    isActive: boolean
+    title: string
+    shortCode: string
+    originalUrl: string
+    expiresAt: string | null
+    isPin: boolean
+    deletedAt: string | null
+    authorId: number
+    visits: Visit[]
+  }
+  
+  export interface Visit {
+    id: number
+    visitedAt: string
+    ipAddress: string
+    country: string | null
+    city: string | null
+    device: string | null
+    browser: string | null
+    source: SourceType
+    linkId: number
+  }
