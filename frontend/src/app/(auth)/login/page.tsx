969b22d95278f16a518b1b587c8309f10c6475ac
Zanyar <167990137+zanyar-D-rahmani@users.noreply.github.com>
2026-08-24 15:19:52 +0330

    Feat: dockerize-frontend
    * feat: set github rule
    
    * chore: initialize frontend project
    
    * chore: initialize frontend project
    
    * fix/fe-README
    
    ---------
    
    Co-authored-by: erfan tolou <erfantolou@gmail.com>
    
    * fix: resolve build errors and improve type safety
    
    * chore: add Dockerfile and .dockerignore for frontend service
    
    ---------
    
    Co-authored-by: mahdi heydari <147157759+Mahdi-heydari@users.noreply.github.com>
    Co-authored-by: erfan tolou <erfantolou@gmail.com>

---
+++ frontend/src/app/(auth)/login/page.tsx
@@ -0,0 +1,8 @@
+// just filling out the file to pass build err docker (it will change)
+export default function Login() {
+  return (
+    <div>
+      <p>Login</p>
+    </div>
+  );
+}
--- frontend/src/types/schema.ts
+++ frontend/src/types/schema.ts
@@ -1,3 +1,48 @@
+<<<<<<< HEAD
+export const SourceType = {
+  DIRECT: "DIRECT",
+  SOCIAL: "SOCIAL",
+  SEARCH: "SEARCH",
+  EMAIL: "EMAIL",
+  OTHER: "OTHER",
+} as const;
+
+export type SourceType = (typeof SourceType)[keyof typeof SourceType];
+export interface User {
+  id: number;
+  createdAt: string;
+  email: string;
+  name: string;
+  links: Link[];
+}
+
+export interface Link {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  isActive: boolean;
+  title: string;
+  shortCode: string;
+  originalUrl: string;
+  expiresAt: string | null;
+  isPin: boolean;
+  deletedAt: string | null;
+  authorId: number;
+  visits: Visit[];
+}
+
+export interface Visit {
+  id: number;
+  visitedAt: string;
+  ipAddress: string;
+  country: string | null;
+  city: string | null;
+  device: string | null;
+  browser: string | null;
+  source: SourceType;
+  linkId: number;
+}
+=======
 export enum SourceType {
     DIRECT = "DIRECT",
     SOCIAL = "SOCIAL",
@@ -40,3 +85,4 @@ export enum SourceType {
     source: SourceType
     linkId: number
   }
+>>>>>>> 0fdd9d3b4639642d855d433150261992eb2ee84f
