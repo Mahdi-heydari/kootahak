aa226e9fadc76502d35264f5091e400787152f82
Zanyar <rahmanikamyar577@gmail.com>
2026-08-30 18:36:01 +0330

    feat: add static footer with basic structure and data

--- frontend/src/contents/landing.ts
+++ frontend/src/contents/landing.ts
@@ -12,3 +12,20 @@ export const mockUser: UserInfo = {
   name: "زانیار رحمانی",
   phone: "09145562747",
 };
+
+export const usefulLinks = [
+  { href: "/1", label: "لینک-1" },
+  {
+    href: "/2",
+    label: "لینک-2",
+    external: true,
+  },
+  { href: "/3", label: "لینک-3" },
+  { href: "/4", label: "لینک-4" },
+];
+
+export const statistics = [
+  { value: "125,340", label: "تعداد لینک‌های کوتاه شده" },
+  { value: "543", label: "کاربران فعال" },
+  { value: "1,230", label: "لینک‌های ایجاد شده امروز" },
+];
